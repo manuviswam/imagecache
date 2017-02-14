a05896939113b5eb19e3c0e2abe6475c3467a9b1
Manu Viswam <evmanu@gmail.com>
2017-02-14 13:34:26 +0530

    Use inputs from query

--- handler/imagehandler.go
+++ handler/imagehandler.go
@@ -2,13 +2,17 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 
 	p "github.com/manuviswam/imagecache/processor"
 )
 
 func ImageHandler(processor *p.ImageProcessor) func (http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		url := r.URL.Query().Get("url")
+		width,_ := strconv.Atoi(r.URL.Query().Get("width"))
+		height,_ := strconv.Atoi(r.URL.Query().Get("height"))
 		w.Header().Set("Content-Disposition", "attachment; filename='picture.png'")
-		w.Write(processor.GetImageWithSize("https://3.bp.blogspot.com/-W__wiaHUjwI/Vt3Grd8df0I/AAAAAAAAA78/7xqUNj8ujtY/s1600/image02.png", 600, 300))
+		w.Write(processor.GetImageWithSize(url, width, height))
 	}
 }
